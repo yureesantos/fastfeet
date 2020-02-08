171c22286689d4ec95ec4c3cf34951995e682b7e
Yure Araújo <yureshell@MacBook-Pro-de-Yure.local>
2020-02-08 10:44:57 -0300

    user update controller

--- src/routes.js
+++ src/routes.js
@@ -10,4 +10,8 @@ const routes = new Router();
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
 export default routes;
