7c13cf39f6c2e16b456c6f24d45ce341a3a065bb
Yure Araújo <yureshell@MacBook-Pro-de-Yure.local>
2020-02-22 13:35:17 -0300

    deliveryman index method and get route

--- src/app/controllers/DeliveryManController.js
+++ src/app/controllers/DeliveryManController.js
@@ -2,6 +2,19 @@ import * as Yup from 'yup';
 import DeliveryMan from '../models/DeliveryMan';
 
 class DeliveryManController {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const deliveryman = await DeliveryMan.findAll({
+      order: ['name'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      attributes: ['id', 'name', 'email'],
+    });
+
+    return res.json(deliveryman);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 import RecipientController from './app/controllers/RecipientController';
 import DeliveryManController from './app/controllers/DeliveryManController';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -23,5 +24,8 @@ routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
 
 routes.post('/deliveryman', DeliveryManController.store);
+routes.get('/deliveryman', DeliveryManController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
